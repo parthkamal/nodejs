c2d8ecc4eb529ea950a8ffa5b4192af4fe92bb8b
parthkamal <kamalparth40@gmail.com>
2023-05-18 18:57:55 +0530

    chapter5 completed

---
+++ chapter5/demo.js
@@ -0,0 +1,8 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+console.log(add(2, 3)); // Output: 5
+
+//in the above function function(a,b) is an anonymous which is then assigned to variable add ;
+
