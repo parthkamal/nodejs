fe82f8caac9aea7e1d240b7027d4344c2074fb49
parthkamal <kamalparth40@gmail.com>
2023-05-18 21:04:06 +0530

    chapter12 completed`

--- chapter8/demo.js
+++ chapter8/demo.js
@@ -1,20 +1,27 @@
 const http = require('http'); 
 const {upperCase} = require('upper-case');
+const fs = require('fs');
 
 const server = http.createServer( (req,res) => {
-	
+
 
 	const value = upperCase('hello from the node js server');
 
 	const html  = `<h1>  ${value} </h1>`;
+	fs.readFile('index.html', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		//now writing the headers 
+		const headers = {
+			'Content-type' : 'text/html'
+		} 
+		res.writeHead(200,headers); 
+		res.write(data); 
+		res.end(); 
 
-	//now writing the headers 
-	const headers = {
-		'Content-type' : 'text/html'
-	} 
-	res.writeHead(200,headers); 
-	res.write(html); 
-	res.end(); 
+	});
 
 });
 
