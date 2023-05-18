2c6036f1b1742dfb2158500e7c6752215ac494e8
parthkamal <kamalparth40@gmail.com>
2023-05-18 22:21:04 +0530

    chapter16 completed

--- chapter15/index.js
+++ chapter15/index.js
@@ -5,15 +5,30 @@ const app = express();
 
 const server = http.createServer(app); 
 
-
-//endpoints
-
-
-app.get('/', (req,res)=> {
-	console.log('client requested');
-	res.json('hello from the server');
+// GET request handler for the root URL
+app.get('/', (req, res) => {
+	res.send('Hello, Express!');
 });
 
+// POST request handler for the '/users' URL
+app.post('/users', (req, res) => {
+	// Handle creating a new user
+	res.send('User created!');
+});
+
+// PUT request handler for the '/users/:id' URL
+app.put('/users/:id', (req, res) => {
+	const userId = req.params.id;
+	// Handle updating the user with the given ID
+	res.send(`User ${userId} updated!`);
+});
+
+// DELETE request handler for the '/users/:id' URL
+app.delete('/users/:id', (req, res) => {
+	const userId = req.params.id;
+	// Handle deleting the user with the given ID
+	res.send(`User ${userId} deleted!`);
+});
 
 
 server.listen(3000, ()=> console.log('server is listening on port 3000'));
