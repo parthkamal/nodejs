9648db998ed9f8df7e6896a41d0a5c7946842fc6
parthkamal <kamalparth40@gmail.com>
2023-05-21 18:31:46 +0530

    chapter26 completed

--- chapter21/routes.js
+++ chapter21/routes.js
@@ -3,34 +3,71 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('./users');
+const { mongo, default: mongoose } = require('mongoose');
 
-
+//body parser for url-encoded for json data
 router.use(express.json());
+router.use(express.urlencoded({extended: false}));
 
 router.get('/', (request, response) => {
-
-    response.setHeader('Content-Type', 'application/json');
-    const message = "hello from the mongodb server side"
-    response.json({ message });
-
-    User.find()
-        .then((users) => {
-            console.log('Users:', users);
-        })
-        .catch((error) => {
-            console.error('Failed to retrieve users:', error);
-        });
-
+    const message = 'hello from the server';
+    res.json({ message });
 });
 
+router.get('/user', (request, response) => {
+    User.find()
+        .then((users) => {
+            response.status(200).json({ users });
+        })
+        .catch((error) => {
+            response.status(400).json({ error });
+        });
+})
 
-router.post('/login',(request, response) => {
-    console.log(request.body);
-    const user = request.body;
+
+router.post('/user', (request, response) => {
+    const {name, email, address} = request.body;
     // console.log(request);
-    const message = 'login post api request';
 
-    response.json({user});
+    User.findOne({email}).then( (user)=> {
+        const message = 'this email is already registered'
+        console.log('found');
+        if (user)response.status(400).json({message});
+
+         
+
+    }).catch( (error) => {
+        console.log('catch');
+        response.status(500).json({error});
+    })
+
+
+    
+        const newUser = new User({
+            _id: new mongoose.Types.ObjectId, 
+            name, 
+            email, 
+            address
+        })
+
+        newUser.save().then((data,error)=> {
+            const message = 'user created successfully';
+            if(data) response.status(200).json({user: data, message});
+            else {
+                console.log('save');
+                response.status(400).json({error});
+            }
+        })
+})
+
+
+router.delete('/user/:id', (request, response) => {
+    const {id} = request.params;
+    User.deleteOne({_id: id}).then( (result )=> {
+        response.status(200).json({result});
+    }).catch((error) =>{
+        console.log({error});
+    })
 })
 
 
--- chapter21/users.js
+++ chapter21/users.js
@@ -1,22 +1,14 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema( {
-// 	_id : mongoose.Schema.Types.ObjectId(), 
-// 	name: String, 
-// 	email: String, 
-// 	address: String
-// });
-
-
-// module.exports = mongoose.model('users',userSchema);
-
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
 	_id: mongoose.Types.ObjectId,
-	name: String, 
-	email : String, 
-	address : String
+	name: String,
+	email:
+	{
+		type: String,
+		unique: true
+	},
+	address: String
 });
 
 
